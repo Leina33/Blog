3381b74f5ac0c8b9e68387d7ffcf8cc79d712e00
Leina33 <dominickrutto@gmail.com>
2019-12-03 00:00:18 +0300

    this is the sub-class Comment with atttributes id,comment, posted and user_id. The id is p.k(uniquely define)

--- app/models.py
+++ app/models.py
@@ -60,16 +60,21 @@ class Post(db.Model):
      def save_post(self):
         db.session.add(self)
         db.session.commit()
-        
-     @classmethod
-    def get_posts(cls,id):
-        posts=Post.query.filter_by(post_id=id).all()
-        return posts
 
     @classmethod
     def get_posts(cls,id):
         posts=Post.query.filter_by(post_id=id).all()
         return posts
+    
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    post_id = db.Column(db.Integer,db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    
     
 
 

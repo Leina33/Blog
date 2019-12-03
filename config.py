9f0b65554b8a0341589d5197df6193da2e6c4f51
Leina33 <dominickrutto@gmail.com>
2019-12-03 08:22:43 +0300

    the file set the configuration enviroment

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+class Config:
+    SECRET_KEY ='dominic'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:dominic@localhost/blog'
+    
+
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
